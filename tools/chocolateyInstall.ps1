f30c56221cea89f52e8d26fb5ff38a22df82178e
Ryan Pavlik <ryan@sensics.com>
2016-09-12 10:41:30 -0500

    Add checksums and proper use of Install-ChocolateyPackage, not trying to be too clever

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,11 @@
 $packageName = 'DotNetCoreSDK'
 $fileType = 'exe'
 $silentArgs = '/quiet'
+$checksumType = 'SHA256'
 $url32 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x86.exe' # 'https://go.microsoft.com/fwlink/?LinkID=809123'
+$checksum32 = '5afd32974fa36606b8e1f0d89ff0a0d4db27e7724c8d8e4b7070a10f74f2dc3e'
 $url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x64.exe' # 'https://go.microsoft.com/fwlink/?LinkID=809122'
+$checksum64 = '967537ba388c4b35cb763c33b8305a9ad598cdc379176bea42a2f0c5a54d80d7'
 $version = '1.0.0.Preview2'
 
 function Test-RegistryValue {
@@ -40,10 +43,8 @@ function CheckDotNetCliInstalled {
 
 if (CheckDotNetCliInstalled) {
     Write-Host "Microsoft .Net Core SDK is already installed on your machine."
-}
-elseif (Get-ProcessorBits(32)) {
-	Install-ChocolateyPackage $packageName $fileType $silentArgs '' $url32
-}
-else {
-	Install-ChocolateyPackage $packageName $fileType $silentArgs '' $url64
+} else {
+    Install-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs \
+        -Url $url32       -Checksum $checksum32   -ChecksumType $ChecksumType \
+        -Url64Bit $url64  -Checksum64 $Checksum64 -ChecksumType64 $checksumType
 }
